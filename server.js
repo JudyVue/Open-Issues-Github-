7c6851070dc9ecf340cc46c5fc113bb4e65fe898
JudyVue <judyvue87@gmail.com>
2017-01-14 13:18:44 -0800

    added eslintrc and eslintignore

--- server.js
+++ server.js
@@ -8,12 +8,21 @@ const express = require('express');
 const requestProxy = require('express-request-proxy');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const githubURL = 'https://api.github.com/';
 
 
+let proxy = (req, res) => {
+  console.log('Routing Github request for ', req.params[0]);
+  (requestProxy({
+    url: `${githubURL}${req.params[0]}`,
+    headers: {Authorization: `token${process.env.GITHUB_TOKEN}`},
+  }))(req, res);
+};
 
+app.get('/github/*', proxy);
 
 app.use(express.static('./'));
 
 app.listen(PORT, () => {
   console.log('Server up on port ', PORT);
-})
+});
