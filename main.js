ba276f9ddf0dc4157a68942d70d4c5440404b1c7
JudyVue <judyvue87@gmail.com>
2017-01-17 15:42:30 -0800

    got submit form working properly when user preseses enter

--- main.js
+++ main.js
@@ -8,17 +8,17 @@
   let daysPassed = 7;
 
 
-  $('button').click(() => {
+  $('form').submit((e) => {
+    e.preventDefault();
     //Empty out the table so it can be replaced with new data
     $('.issue-list').empty();
-
     //Empty out the array for same reason
     issues.data = [];
 
     //Get URL entered in input field and parse out the user name and repo in the callback function
     issueView.getInputURL(helpers.parseInputURL);
 
-    //Make Github API request now that we have a user name and repo name from the input field, set the returned data to local storage, calculate #issues and render to to the page, OR show the bad request h5 if user did not input valid URL
+    //Make Github API request now that we have a user name and repo name from the input field, set the returned data to local storage, calculate #issues and render to the page, OR show the bad request msg if user did not input valid URL
     issues.fetchData(helpers.setLocalStorage, () => {
       helpers.getNumberOfIssues(daysPassed, '.issue-template', issueView.appendData);
     }, issueView.badRequest);
